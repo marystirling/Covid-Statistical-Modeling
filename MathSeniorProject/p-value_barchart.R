4e0adaae91c47a8b226282c4e6d8d0476def80b4
marystirling <marystirlingbrown@gmail.com>
2022-02-18 00:30:36 -0600

    updated barchart to be horizontal

--- MathSeniorProject/p-value_barchart.R
+++ MathSeniorProject/p-value_barchart.R
@@ -3,7 +3,37 @@ library(ggplot2)
 all = read_excel('C:\\Users\\marys\\OneDrive\\Documents\\Covid-Statistical-Modeling\\MathSeniorProject\\Data.xlsx', 'Hypothesis Testing')
 print(all)
 
-stock_name = as.list(all$`Index/Stock`)
+stock_name <- all$`Index/Stock`
+p_value <- all$`p-value`
+df <- data.frame(stock_name, p_value)
+#stock_name = factor(stock_name, levels = c("Dow Jones", "S&P 500", "NASDAQ", "MSFT", "CRM", "AAPL", "UNH", "AMGN", "JNJ", "GS", "V", "AXP", "BA", "CAT", "HON", "HD", "MCD", "DIS"), ordered = TRUE)
+
+ggplot(df, aes(stock_name, all$`p-value`)) + 
+  geom_bar(stat = "identity", fill = "light blue") + coord_flip() + 
+  theme(axis.text.x = element_text(color="black",
+                                   size=14, angle=0),
+        axis.text.y = element_text(color="black",
+                                   size=14, angle=0),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()) + scale_x_discrete(limits = stock_name) 
+
+rev_stock_name <- all$Rev_Name
+rev_p_value <- all$Rev_p_value
+rev_df <- data.frame(rev_stock_name, rev_p_value)
+#stock_name = factor(stock_name, levels = c("Dow Jones", "S&P 500", "NASDAQ", "MSFT", "CRM", "AAPL", "UNH", "AMGN", "JNJ", "GS", "V", "AXP", "BA", "CAT", "HON", "HD", "MCD", "DIS"), ordered = TRUE)
+
+ggplot(rev_df, aes(rev_stock_name, rev_p_value)) + 
+  geom_bar(stat = "identity", fill = "light blue") + coord_flip() + 
+  theme(axis.text.x = element_text(color="black",
+                                   size=14, angle=0),
+        axis.text.y = element_text(color="black",
+                                   size=14, angle=0),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()) + scale_x_discrete(limits = rev_stock_name)
+
+
+coord_flip() + stock_name = as.list(all$`Index/Stock`)
+
 p_value = as.numeric(all$`p-value`)
 
 barplot(height=all$`p-value`, names = all$`Index/Stock`, 
@@ -12,16 +42,20 @@ barplot(height=all$`p-value`, names = all$`Index/Stock`,
         main="p-values", 
         ylim=c(0,1),
         las = 2,
-        horiz = TRUE
+        #horiz = TRUE
 )
 
+ggplot(df, aes(x = stock_name, y = p_value))
 
+df$all..p.value. = as.numeric(df$all..p.value.)
 options(repr.plot.width=8, repr.plot.height=3)
-ggplot(y, aes(x = stock_name, y = p_value, main="Car Distribution")) +
+ggplot(data, aes(x = stock_name, y = p_value, main="Car Distribution")) +
   geom_bar(stat = "identity") +
-  coord_flip() + scale_y_continuous(name="Average Trip Duration (in seconds)") +
+  coord_flip() + scale_y_discrete(name="Average Trip Duration (in seconds)") +
   scale_x_discrete(name="Start Station") +
   theme(axis.text.x = element_text(face="bold", color="#008000",
                                    size=8, angle=0),
         axis.text.y = element_text(face="bold", color="#008000",
                                    size=8, angle=0))
+
+ggplot(data, aes(x = stock_name, y = p_value)) + geom_point() + scale_y_continuous(limits = c(0, 1))
