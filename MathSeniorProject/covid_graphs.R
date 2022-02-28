0706ab89c69f6f20107f221ea74f19263a7f2f6b
marystirling <marystirlingbrown@gmail.com>
2022-02-28 14:25:34 -0600

    limited date version of graph of covid vs dow

--- MathSeniorProject/covid_graphs.R
+++ MathSeniorProject/covid_graphs.R
@@ -39,9 +39,40 @@ dow <-as.numeric(unlist(dow_list))
 sp <- as.numeric(unlist(sp_list))
 nasdaq <- as.numeric(unlist(nasdaq_list))
 
+limited_date <-c()
 
+df_dow <- c()
+df_covid_daily <- c()
 
+counter = 0
+for (i in dow){
+  if (counter >= 581){
+    df_dow <- append(df_dow,i)
+  }
+  counter = counter + 1
+}
+counter = 0
+for (i in covid_daily){
+  if (counter >= 581){
+    df_covid_daily <- append(df_covid_daily,i)
+  }
+  counter = counter + 1
+}
 
+counter = 0
+print(length(date))
+for (i in 1:length(date)){
+  if (counter >=581){
+    limited_date <- append(limited_date, date[[i]])
+  }
+  counter = counter + 1
+}
+
+print(limited_date)
+print(df_dow)
+print(df_covid_daily)
+
+df_a <- data.frame(df_covid_daily, df_dow)
 
 
 df <- data.frame(covid_daily, dow)
@@ -61,4 +92,20 @@ ggplot(df, aes(x=date)) +
     # Add a second axis and specify its features
     sec.axis = sec_axis(~.*coeff, name="Dow Jones") 
   )
+
+coeff = 15
+ggplot(df_a, aes(x=limited_date)) +
+  
+  geom_line( aes(y=df_covid_daily/coeff), color = "blue") + 
+  geom_line( aes(y=df_dow), color = "red") + # Divide by 10 to get the same range than the temperature
+  
+  scale_y_continuous(
+    
+    # Features of the first axis
+    name = "Daily Cases of COVID-19",
+    
+    # Add a second axis and specify its features
+    sec.axis = sec_axis(~.*coeff, name="Dow Jones") 
+  )
+f
 
