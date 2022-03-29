1c05cd8c6c408d21046c89386530afe3ae5fde70
marystirling <marystirlingbrown@gmail.com>
2022-03-29 13:25:38 -0500

    updated statistical models with limited date for start of covid

--- MathSeniorProject/statistical_modeling.R
+++ MathSeniorProject/statistical_modeling.R
@@ -32,20 +32,79 @@ dow <-as.numeric(unlist(dow_list))
 sp <- as.numeric(unlist(sp_list))
 nasdaq <- as.numeric(unlist(nasdaq_list))
 
-print(dow)
-print(sp)
-print(nasdaq)
-print(covid_daily)
-print(vacc_daily)
 
-# linear regression model for Dow Jones
+# creating new list for each value to get it to have start date of January 1, 2020
+df_dow <- c()
+df_covid_daily <- c()
+df_sp <- c()
+df_nasdaq <- c()
+df_vacc_daily <- c()
+
+# appends value onto new list starting from January 1, 2020 for each variable below
+counter = 0
+for (i in dow){
+  if (counter >= 581){
+    df_dow <- append(df_dow,i)
+  }
+  counter = counter + 1
+}
+
+counter = 0
+for (i in sp){
+  if (counter >= 581){
+    df_sp <- append(df_sp,i)
+  }
+  counter = counter + 1
+}
+
+counter = 0
+for (i in nasdaq){
+  if (counter >= 581){
+    df_nasdaq <- append(df_nasdaq,i)
+  }
+  counter = counter + 1
+}
+
+
+counter = 0
+for (i in covid_daily){
+  if (counter >= 581){
+    df_covid_daily <- append(df_covid_daily,i)
+  }
+  counter = counter + 1
+}
+
+counter = 0
+for (i in vacc_daily){
+  if (counter >= 581){
+    df_vacc_daily <- append(df_vacc_daily,i)
+  }
+  counter = counter + 1
+}
+print(df_vacc_daily)
+
+
+# linear regression model for Dow Jones from June 1, 2018 to December 31, 2021
 dow.lm<-lm(dow ~ covid_daily + vacc_daily)
 summary(dow.lm)
 
-# linear regression model for S&P 500
+# linear regression model for Dow Jones from January 1, 2020 to December 31, 2021
+df_dow.lm <- lm(df_dow ~ df_covid_daily + df_vacc_daily)
+summary(df_dow.lm)
+
+# linear regression model for S&P 500 from June 1, 2018 to December 31, 2021
 sp.lm<-lm(sp ~ covid_daily + vacc_daily)
 summary(sp.lm)
 
-# linear regression model for Nasdaq
+# linear regression model for S&P 500 from January 1, 2020 to December 31, 2021
+df_sp.lm <- lm(df_sp ~ df_covid_daily + df_vacc_daily)
+summary(df_sp.lm)
+
+
+# linear regression model for Nasdaq from June 1, 2018 to December 31, 2021
 nasdaq.lm<-lm(nasdaq ~ covid_daily + vacc_daily)
 summary(nasdaq.lm)
+
+# linear regression model for Nasdaq from January 1, 2020 to December 31, 2021
+df_nasdaq.lm <- lm(df_nasdaq ~ df_covid_daily + df_vacc_daily)
+summary(df_nasdaq.lm)
